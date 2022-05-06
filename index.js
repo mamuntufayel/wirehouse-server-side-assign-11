a31c731db2ee9e73b928fcf21d9d736c60915e33
mamuntufayel <mamuntuffa@gmail.com>
2022-05-06 01:46:54 -0400

    edit a product added to server

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 // getting
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const res = require("express/lib/response");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fis18.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -38,13 +39,33 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    // Make change to a data object
+    app.put("/product/:id", async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          price: data.price,
+          description: data.description,
+          img: data.img,
+          quantity: data.quantity,
+          supplier: data.supplier,
+        },
+      };
+      const result = await ProductCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
-app.get("/", (req, res) => {
-  res.send("server is running");
-});
 
 app.listen(port, () => {
   console.log("listening to port", port);
